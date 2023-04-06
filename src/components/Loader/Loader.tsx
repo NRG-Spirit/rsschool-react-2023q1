c768f2781164677db96dc5a1f1f7e049d0722b91
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2023-04-06 19:01:49 +0300

    feat: add spinner when loading

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,5 +2,19 @@ import React from 'react';
 import './Loader.css';
 
 export default function Loader() {
-  return <div className="loader">Loading</div>;
+  return (
+    <div className="loader">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      L O A D I N G . . .
+    </div>
+  );
 }
