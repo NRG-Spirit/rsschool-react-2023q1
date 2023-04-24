051c828a09cb38795c146b64ff8e107d7bd1d615
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2023-04-24 16:54:34 +0300

    feat: implement cypress

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,22 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: './src/tests/coverage',
+    },
   },
 });
